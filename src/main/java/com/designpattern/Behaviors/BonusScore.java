81e8c99b8042e95d5b83bffb9f0527d23e8ab69a
Zulfathi Hanafi <fathiimran7@gmail.com>
2023-12-28 10:46:08 +0800

    Add bonus behaviors

--- src/main/java/com/designpattern/Behaviors/BonusScore.java
+++ src/main/java/com/designpattern/Behaviors/BonusScore.java
@@ -1,11 +1,20 @@
 package com.designpattern.Behaviors;
 
+import com.designpattern.Singleton.Game;
+import com.designpattern.Singleton.Logger;
+
+import javafx.scene.control.Label;
+
 public class BonusScore implements ScoreBehavior{
 
     @Override
     public void handleScore() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'handleScore'");
+        Game game = Game.getInstance();
+        game.setScore(game.getScore() + 5);
+        
+        Logger.getInstance().log("Plus Score! New Score : " + Game.getInstance().getScore());
+        Label actualScoreLabel = (Label) Game.getInstance().getScene().lookup("#actualScoreLabel");
+        actualScoreLabel.setText(Integer.toString(Game.getInstance().getScore()));
     }
     
 }
